089d1134f530688e2bba5548cc05d3c2ccf8312b
jordanu <jordanelcracken@gmail.com>
2025-03-16 18:38:17 -0500

    feat: add named constructor

--- monorepo/typescript/hexagonal-architecture/book/domain/Book.ts
+++ monorepo/typescript/hexagonal-architecture/book/domain/Book.ts
@@ -1,25 +1,25 @@
-class Book{
-  private title: string;
-  private author: string;
+class Book {
+    private title: string;
+    private author: string;
 
-  constructor(title: string, author: string) {
-      this.title = title;
-      this.author = author;
-  }
-  public emptyConstructor():Book {
-    const book:Book = new Book("","");
-    return book ;
-  }
-  getTitle(): string {
-      return this.title;
-  }
-  setTitle(title: string): void {
-      this.title = title;
-  }
-  getAuthor(): string {
-      return this.author;
-  }
-  setAuthor(author: string): void {
-      this.author = author;
-  }
+    constructor(title: string, author: string) {
+        this.title = title;
+        this.author = author;
+    }
+    public static create(title: string, author: string): Book {
+        const book = new Book(title, author);
+        return book;
+    }
+    getTitle(): string {
+        return this.title;
+    }
+    setTitle(title: string): void {
+        this.title = title;
+    }
+    getAuthor(): string {
+        return this.author;
+    }
+    setAuthor(author: string): void {
+        this.author = author;
+    }
 }
