5967760e0f2305cc19684e90e1dae6fce732a500
jorda <jordanelcracken@gmail.com>
2025-03-23 17:59:05 -0500

    feat: add testing

--- monorepo/typescript/ddd-architecture/src/apps/mooc/backend/controllers/CoursePutController.ts
+++ monorepo/typescript/ddd-architecture/src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -1,10 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { Controller } from "./Controller";
+import { CourseCreator } from "../../../../Contexts/Mooc/Courses/application/CourseCreator";
 
 export default class CoursePutController implements Controller {
+  constructor(private courseCreator: CourseCreator){}
   async run(req: Request, res: Response,next:NextFunction): Promise<void> {
-    // res.status(httpStatus.CREATED).json({ status: "ok" });
-    throw new Error("Method is not implemented");
+    const {id, name, duration} = req.body
+    await this.courseCreator.run(id, name, duration);
+    res.status(httpStatus.CREATED).json({ status: "ok" });
   }
 }
--- monorepo/typescript/ddd-architecture/tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
+++ monorepo/typescript/ddd-architecture/tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
@@ -1,26 +1,33 @@
-import assert from "assert";
+import assert from 'assert';
+import { AfterAll, BeforeAll, Given, Then } from '@cucumber/cucumber';
+import request from 'supertest';
+import { MoocBackendApp } from '../../../../../../src/apps/mooc/backend/MoocBackendApp';
+import { App } from 'supertest/types';
 
-import { AfterAll, BeforeAll, Given, Then } from "@cucumber/cucumber"
-import request from "supertest"
-import { MoocBackendApp } from "../../../../../../src/apps/mooc/backend/MoocBackendApp";
-import { App } from "supertest/types";
-
-let _request:request.Test;
+let _request: request.Test;
 let application: MoocBackendApp;
-let _respose: request.Response;
+let _response: request.Response;
+//TODO: fix the application.httpServer in all cases
+Given('I send a GET request to {string}', (route: string) => {
+  _request = request(application.httpServer as App).get(route);
+});
 
+Then('the response status code should be {int}', async (status: number) => {
+  _response = await _request.expect(status);
+});
 
-BeforeAll(async ()=>{
+Given('I send a PUT request to {string} with body:', (route: string, body: string) => {
+  _request = request(application.httpServer as App).put(route).send(JSON.parse(body));
+});
+
+Then('the response should be empty', () => {
+  assert.deepStrictEqual(_response.body, {});
+});
+
+BeforeAll(async () => {
   application = new MoocBackendApp();
   await application.start();
-})
-
-Given("I send a GET request to {string}",(route:string)=>{
-  _request = request(application.httpServer).get(route);
-})
-Then('the response status code should be {int}', async (status:number)=>{
-  await _request.expect(status)
-})
+});
 
 AfterAll(async () => {
   await application.stop();
