9dc3ec2bd90a422fa8a9d40a46a28a91320b28d2
jorda <jordanelcracken@gmail.com>
2025-03-23 16:30:45 -0500

    feat: fix

--- monorepo/typescript/ddd-architecture/src/apps/mooc/backend/controllers/CoursePutController.ts
+++ monorepo/typescript/ddd-architecture/src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -4,6 +4,7 @@ import { Controller } from "./Controller";
 
 export default class CoursePutController implements Controller {
   async run(req: Request, res: Response,next:NextFunction): Promise<void> {
-    res.status(httpStatus.OK).json({ status: "ok" });
+    // res.status(httpStatus.CREATED).json({ status: "ok" });
+    throw new Error("Method is not implemented");
   }
 }
