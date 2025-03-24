6a894fa72dfe49a5c44a8453a1c2fc594e32a14e
jorda <jordanelcracken@gmail.com>
2025-03-23 19:19:27 -0500

    test: add test for fileCourseRepository

--- monorepo/typescript/ddd-architecture/src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.ts
+++ monorepo/typescript/ddd-architecture/src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.ts
@@ -1,4 +1,4 @@
-import BSON from 'bson';
+import BSON, { deserialize } from 'bson';
 import fs from 'fs';
 import { Course } from '../../domain/Course';
 import { CourseRepository } from '../../domain/CourseRepository';
@@ -9,7 +9,11 @@ export class FileCourseRepository implements CourseRepository {
   async save(course: Course): Promise<void> {
     fs.promises.writeFile(this.filePath(course.id), BSON.serialize(course));
   }
-
+  async search(couseId: string): Promise<Course> {
+    const courseData = await fs.promises.readFile(this.filePath(couseId));
+    const { _id:id, _name:name, _duration:duration } = deserialize(courseData);
+    return new Course(id, name, duration);
+  }
   private filePath(id: string): string {
     return `${this.FILE_PATH}.${id}.repo`;
   }
