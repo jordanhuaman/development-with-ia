3f6fc524e78e6535ef586251a9f6dc5d7ca01dda
jorda <jordanelcracken@gmail.com>
2025-04-01 11:54:17 -0500

    feat: adding simple validator schema

--- monorepo/typescript/ddd-architecture/src/apps/mooc/backend/routes/course.route.ts
+++ monorepo/typescript/ddd-architecture/src/apps/mooc/backend/routes/course.route.ts
@@ -1,9 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
 import container from "../dependency-inyection";
 import CoursePutController from "../controllers/CoursePutController";
+import { body } from "express-validator";
+import { validateReqSchema } from ".";
 
 export const register = async (router: Router) => {
+
+  const reqSchema = [
+    body("id").exists().isString(),
+    body("name").exists().isString(),
+    body("duration").exists().isString()
+  ];
+
   const contai = await container();
   const putcontroller: CoursePutController = await contai.get("Apps.mooc.controllers.CoursePutController");
-  router.put("/courses/:id", (req: Request, res: Response,next:NextFunction) => putcontroller.run(req, res, next))
+  router.put("/courses/:id",reqSchema,validateReqSchema, (req: Request, res: Response,next:NextFunction) => putcontroller.run(req, res, next))
 }
--- monorepo/typescript/ddd-architecture/src/apps/mooc/backend/routes/index.ts
+++ monorepo/typescript/ddd-architecture/src/apps/mooc/backend/routes/index.ts
@@ -1,13 +1,27 @@
-import { Router } from "express";
-import glob, {globSync} from "glob";
+import { NextFunction, Request, Response, Router } from "express";
+import glob, { globSync } from "glob";
 import path from "path";
 
+import { ValidationError, validationResult } from "express-validator";
+import httpStatus from "http-status"
+
 export function registerRoutes(router: Router) {
-  const routes = globSync("src/**/*.route.ts",{ignore:"node_modules/**"})
+  const routes = globSync("src/**/*.route.ts", { ignore: "node_modules/**" })
   routes.map((route) => register(route, router));
 }
 
 function register(routePath: string, router: Router) {
   const route = require(path.resolve(routePath));
   route.register(router);
+}
+
+export function validateReqSchema(req: Request, res: Response, next: NextFunction) {
+  const validationsErrors = validationResult(req);
+  if (validationsErrors.isEmpty()) {
+    return next();
+  }
+
+  const errors = validationsErrors.array().map((err) => ({ [err.path]: err.msg }));
+
+  res.status(httpStatus.UNPROCESSABLE_ENTITY).json({ errors })
 }
