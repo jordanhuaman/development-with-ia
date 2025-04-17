debf7f20127d15ea21365c54823665192e248789
jorda <jordanelcracken@gmail.com>
2025-04-17 12:53:26 -0500

    feat: update the last test using object mother

--- monorepo/typescript/ddd-architecture/tests/Contexts/Mooc/Courses/application/CreateCourse.test.ts
+++ monorepo/typescript/ddd-architecture/tests/Contexts/Mooc/Courses/application/CreateCourse.test.ts
@@ -1,9 +1,5 @@
 import { CourseCreator } from "../../../../../src/Contexts/Mooc/Courses/application/CourseCreator";
-import { Course } from "../../../../../src/Contexts/Mooc/Courses/domain/Course";
-import { CourseDuration } from "../../../../../src/Contexts/Mooc/Courses/domain/CourseDuration";
-import { CourseName } from "../../../../../src/Contexts/Mooc/Courses/domain/CourseName";
 import { CourseNameLenghtExceded } from "../../../../../src/Contexts/Mooc/Courses/domain/CouseNameLenghtExcceded";
-import { CourseId } from "../../../../../src/Contexts/Mooc/Shared/domain/Courses/CourseId";
 import { CourseRepositoryMock } from "../__mocks__/CourseRepositoryMock";
 import { CourseMother } from "../domain/CourseMother";
 import { CreateCourseRequestMother } from "./CreateCourseRequestMother";
@@ -28,18 +24,12 @@ describe("CouseCreator", () => {
   })
 
   it('should throw error if course name length is exceeded', async () => {
-    const id = '0766c602-d4d4-48b6-9d50-d3253123275e';
-    const name = 'some-name'.repeat(30);
-    const duration = 'some-duration';
-
     expect(() => {
-      const course = new Course({
-        id: new CourseId(id),
-        name: new CourseName(name),
-        duration: new CourseDuration(duration)
-      });
+      const request = CreateCourseRequestMother.invalidRequest();
 
-      creator.run({ id, name, duration });
+      const course = CourseMother.fromRequest(request);
+
+      creator.run(request);
 
       repository.assertSaveHaveBeenCalled(course);
     }).toThrow(CourseNameLenghtExceded);
